0d79608452bb6627743648c08adc47c663825bf7
johnworker <78348941+johnworker@users.noreply.github.com>
2022-06-01 20:08:13 +0800

    程式設定

---
+++ Assets/script/LearnMethod.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearnMethod : MonoBehaviour
+{
+    // 方法語法
+    // 回傳類型 方法自訂名稱 () { 方法內容 }
+    // 無回傳 void
+    // 方法名稱 Unity 習慣用大寫開頭
+    void Test ()
+    {
+        // 輸出(內容)
+        print("hello world");
+    }
+}
