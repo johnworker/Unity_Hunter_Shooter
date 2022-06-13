e2a36cf3edca653ef2a671793286908f619c2c1b
johnworker <78348941+johnworker@users.noreply.github.com>
2022-06-13 19:40:03 +0800

    添加參數和物件

--- Assets/script/LearnMethod.cs
+++ Assets/script/LearnMethod.cs
@@ -19,10 +19,18 @@ public class LearnMethod : MonoBehaviour
         // 方法名稱
         Text();
         PrintColorText();
+        print("傳回10方法結果：" + ReturnTen());
+        print("商品總價：" + CalculatePrice());
+
+        Shoot("火球");
+        Shoot("電球");
+        Shoot("冰球", "滋滋滋");
+
     }
 
     // 方法語法
-    // 回傳類型 方法自訂名稱 () { 方法內容 }
+    // 回傳類型 方法自訂名稱 (參數1, 參數2, ...) { 方法內容 }
+    // ↗上述之參數通常不建議寫太多，但還是有少數遊戲是有寫超過10個參數
     // 無回傳 void
     // 方法名稱 Unity 習慣用大寫開頭
     private void Text()
@@ -32,10 +40,52 @@ public class LearnMethod : MonoBehaviour
 
     }
 
+    #region 方法練習
     private void PrintColorText()
     {
         print("<color=yellow>黃色</color>");
         print("<color=red>紅色</color>");
         print("<color=#003366>藍色</color>");
+    }
+
+    // 傳回方法
+    // 必須搭配return
+    private int ReturnTen()
+    {
+        // 傳回 資料 (此資料類型必須與傳回類型相同)
+        return 10;
+    }
+
+    // 商店系統：99元，計算全部的商品價格
+    public int countProduct = 10;
+    public int countPrice = 99;
+
+    private int CalculatePrice()
+    {
+        return countProduct * countPrice;
+    }
+
+    #endregion
+
+    //發射火球、發射雷電
+    //撥放音效
+    private void shootFire()
+    {
+        print("發射火球");
+        print("播放音效");
+    }
+
+    private void shotLighting()
+    {
+        print("發射電球");
+        print("播放音效");
+    }
+
+
+    //參數語法：參數類型 參數名稱 指定 預設值
+    private void Shoot(string type, string sound = "咻咻咻")
+    {
+        print("發射：" + type);
+        print("音效：" +sound);
     }
 }
