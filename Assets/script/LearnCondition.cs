737aa2c47bf2e5da782af3faf68e9212734abca4
Projecttogather <107772968+Projecttogather@users.noreply.github.com>
2022-06-27 21:20:22 +0800

    判斷式練習

--- Assets/script/LearnCondition.cs
+++ Assets/script/LearnCondition.cs
@@ -9,6 +9,9 @@ using UnityEngine;
 /// </summary>
 public class LearnCondition : MonoBehaviour
 {
+    public bool openDoor;
+    public int combo;
+
     #region if 判斷式
 
     // if 語法：
@@ -19,6 +22,45 @@ public class LearnCondition : MonoBehaviour
         {
             print("我在判斷式 if 內");
         }
+    }
+
+    private void Update()
+    {
+        // 如果 openDoor 等於true 就開門 ， 否則就關門
+        // 如果
+        // if 語法：
+        // if (布林值) {布林值等於 true 會執行}
+        // 否則
+        // else {布林值等於 false 會執行}
+
+        if (openDoor)
+        {
+            print("開門");
+        }
+        else
+        {
+            print("關門");
+        }
+
+        // else if () {}
+        // 可無限數量
+        // 連擊數 < 100 攻擊力 + 0%
+        // 連擊數 >= 100 攻擊力 + 10%
+        // 連擊數 >= 200 攻擊力 + 20%
+
+        if (combo < 100)
+        {
+            print("攻擊力 + 0%");
+        }
+        else if (combo >= 200)
+        {
+            print("攻擊力 + 20%");
+        }
+        else if (combo >= 100)
+        {
+            print("攻擊力 + 10%");
+        }
+
     }
 
     #endregion
