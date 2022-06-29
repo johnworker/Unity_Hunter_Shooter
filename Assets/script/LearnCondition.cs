0d8c0f64bcc5e2129b79dd60f176d795e7afe844
johnworker <78348941+johnworker@users.noreply.github.com>
2022-06-29 20:05:04 +0800

    添加內容

--- Assets/script/LearnCondition.cs
+++ Assets/script/LearnCondition.cs
@@ -11,6 +11,21 @@ public class LearnCondition : MonoBehaviour
 {
     public bool openDoor;
     public int combo;
+    public string prop;
+
+    // 定義列舉內容
+    // 定義欄位
+
+    // 列舉語法:
+    // 修飾詞 列舉 列舉名稱 ( 列舉內容 )
+    public enum StatePlayer
+    {
+        Idle, Walk, Run, Hurt, Attack, Dead
+    }
+
+    // 定義欄位
+    // 修飾詞 列舉名稱 欄位名稱
+    public StatePlayer stateplayer;
 
     #region if 判斷式
 
@@ -60,8 +75,68 @@ public class LearnCondition : MonoBehaviour
         {
             print("攻擊力 + 10%");
         }
+        #endregion
+
+        #region Switch 判斷式
+        // switch 語法：
+        // switch (比較的運算式)
+        // {
+        //      case 值:
+        //          程式內容
+        //          break;
+        // }
+
+        switch (prop)
+        {
+            case "紅色藥水":
+                print("補血");
+                break;
+
+            case "藍色藥水":
+                print("補魔");
+                break;
+
+            case "黃色藥水":
+                print("補體");
+                break;
+
+            // 可省略
+            default:
+                print("沒有此道具");
+                break;
+
+
+
+        }
+
+        switch (stateplayer)
+        {
+            case StatePlayer.Idle:
+                print("等待");
+                break;
+            case StatePlayer.Walk:
+                print("走路");
+                break;
+            case StatePlayer.Run:
+                print("跑步");
+                break;
+            case StatePlayer.Hurt:
+                print("受傷");
+                break;
+            case StatePlayer.Attack:
+                print("攻擊");
+                break;
+            case StatePlayer.Dead:
+                print("死亡");
+                break;
+            default:
+                print("沒有此狀態");
+                break;
+        }
+        #endregion
 
     }
 
-    #endregion
+
+
 }
--- Assets/script/SystemControl.cs
+++ Assets/script/SystemControl.cs
@@ -6,9 +6,36 @@ using UnityEngine;
 
 namespace LEO
 { 
+    /// <summary>
+    /// 玩家控制系統
+    /// 物件旋轉、彈珠發射
+    /// </summary>
 public class SystemControl : MonoBehaviour
 {
+        #region 資料
+        // GameObject 遊戲物件
+        // 存放階層面板內或專案內的物件
 
-}
+        // 箭頭
+        [Header("箭頭")]
+        public GameObject arraw;
+        // 旋轉速度
+        [Header("旋轉速度"), Range(0, 500)]
+        public float speedTurn = 10.5f;
+        // 彈珠預置物
+        [Header("彈珠預置物")]
+        public GameObject mrable;
+        // 彈珠可發射的總數
+        [Header("彈珠可發射的總數"), Range(0, 50)]
+        public int canShootMarbleTotle = 15;
+        #endregion
+
+        #region 事件
+        #endregion
+
+        #region 方法
+        #endregion
+
+    }
 
 }
