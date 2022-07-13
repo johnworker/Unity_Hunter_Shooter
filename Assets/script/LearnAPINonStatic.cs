6ce45935cef4c3849edf98a3408a9d0f54fda758
johnworker <78348941+johnworker@users.noreply.github.com>
2022-07-13 19:33:02 +0800

    添加內容

---
+++ Assets/script/LearnAPINonStatic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 學習 非靜態 API
+/// API 文件上沒有 static
+/// </summary>
+public class LearnAPINonStatic : MonoBehaviour
+{
+    public Transform transA;
+
+    private void Start()
+    {
+        // 非靜態屬性 properties
+        // 1.取得 get
+        //
+        //
+        //
+    }
+}
