eccfe21d2dc15d06006ba0cdc19515e2780c59e7
johnworker <78348941+johnworker@users.noreply.github.com>
2022-07-27 19:51:14 +0800

    添加說明

--- Assets/script/SystemSpawn.cs
+++ Assets/script/SystemSpawn.cs
@@ -13,12 +13,27 @@ namespace Leo
         #region 資料
         [Header("怪物陣列"), SerializeField]
         private GameObject[] goEnemys;
+        [Header("生成格子第二排座標"), SerializeField]
+        private Transform[] traSecondPlace;
         #endregion
 
         #region 事件
+        private void Awake()
+        {
+            SpawnRandomEnemy();
+        }
         #endregion
 
         #region 方法
+        /// <summary>
+        /// 生成隨機敵人
+        /// </summary>
+        private void SpawnRandomEnemy()
+        {
+            int min = 2;
+            int max = traSecondPlace.Length;
+            print(Random.Range(min, max));
+        }
         #endregion
 
     }
