d8df7ade69205f99d314e182141f0c49bc3c4f4c
johnworker <78348941+johnworker@users.noreply.github.com>
2022-08-08 19:49:46 +0800

    添加遊戲怪物血量和受傷

--- Assets/script/SystemSpawn.cs
+++ Assets/script/SystemSpawn.cs
@@ -1,24 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;       // 資料結構清單
-using System.Linq;                      // 查詢語言
 using UnityEngine;
+using System.Collections.Generic;   // 資料結構 清單 List
+using System.Linq;                  // 查詢語言
 
-
-namespace Leo
-{ 
+namespace KID
+{
     /// <summary>
     /// 生成怪物系統
     /// </summary>
     public class SystemSpawn : MonoBehaviour
-        {
+    {
         #region 資料
+        // [] 陣列
+        // SerializeField 將私人欄位顯示
         [Header("怪物陣列"), SerializeField]
         private GameObject[] goEnemys;
         [Header("生成格子第二排座標"), SerializeField]
         private Transform[] traSecondPlace;
 
         [SerializeField]
-        private List<Transform> listScendPlace = new List<Transform>();
+        private List<Transform> listSecondPlace = new List<Transform>();
         #endregion
 
         #region 事件
@@ -38,41 +38,52 @@ namespace Leo
             int max = traSecondPlace.Length;
 
             int randomCount = Random.Range(min, max);
-            print("怪物隨機數量：" + randomCount);
+            print("隨機怪物數量：" + randomCount);
 
-            // 清單 = 陣列.轉換清單();
-            listScendPlace = traSecondPlace.ToList();
+            // 清單 = 陣列.轉為清單();
+            listSecondPlace = traSecondPlace.ToList();
 
             // 隨機物件
             System.Random random = new System.Random();
-            // 清單 = 清單的排序(每個清單內容 => 隨機排序).轉為清單
-            listScendPlace = listScendPlace.OrderBy(x => random.Next()).ToList();
+            // 清單 = 清單 的 排序(每個清單內容 => 隨機排序) 轉為清單
+            listSecondPlace = listSecondPlace.OrderBy(x => random.Next()).ToList();
 
             int sub = traSecondPlace.Length - randomCount;
             print("要扣除的數量：" + sub);
 
-            // 迴圈 刪除 要扣除的數量
+            // 迴圈 刪除 要扣掉的數量
             for (int i = 0; i < sub; i++)
             {
                 // 刪除第一個清單資料
-                listScendPlace.RemoveAt(0);
+                listSecondPlace.RemoveAt(0);
             }
 
-            // 生成所有怪物
-            for (int i = 0; i < listScendPlace.Count; i++)
+            // 生成所有怪物與彈珠一顆
+            for (int i = 0; i < listSecondPlace.Count; i++)
             {
-                // 隨機怪物
-                int randomIndex = Random.Range(0, goEnemys.Length);
-                // 生成怪物
-                Instantiate(
-                    goEnemys[randomIndex],
-                    listScendPlace[i].position,
-                    Quaternion.identity
-                    );
+                // 如果 i 等於 0 生成彈珠
+                if (i == 0)
+                {
+                    Instantiate(
+                    goMarble,
+                    listSecondPlace[i].position,
+                    Quaternion.identity);
+                }
+                else
+                {
+                    // 隨機怪物
+                    int randomIndex = Random.Range(0, goEnemys.Length);
+                    // 生成怪物
+                    Instantiate(
+                        goEnemys[randomIndex],
+                        listSecondPlace[i].position,
+                        Quaternion.identity);
+                }
             }
         }
         #endregion
 
+        [Header("格子 彈珠"), SerializeField]
+        private GameObject goMarble;
     }
-
 }
